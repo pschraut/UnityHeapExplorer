d356870648f1304ae74fff795b248216e7a8a368
Peter Schraut <p-bitbucket@console-dev.de>
2018-06-07 19:31:41 +0200

    Added "Heap Explorer Lite" Unity project

---
+++ UnityDLL/Code/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+[assembly: AssemblyIsEditorAssembly]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Peter Schraut")]
+[assembly: AssemblyProduct("HeapExplorer")]
+[assembly: AssemblyCopyright("Copyright 2017-2018 Peter Schraut")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyTitle("Heap Explorer")]
+[assembly: Guid("7a990e84-7c9e-495a-a6ce-00ad90e66f32")]
+
