b60c1f5a6537ece031b5cd7362004a47e4ddc860
Peter Schraut <p-bitbucket@console-dev.de>
2018-07-08 19:13:29 +0200

    Bump version to 2.3

--- Unity/Assets/HeapExplorer/Editor/Scripts/HeGlobals.cs
+++ Unity/Assets/HeapExplorer/Editor/Scripts/HeGlobals.cs
@@ -7,7 +7,7 @@ namespace HeapExplorer
     public static class HeGlobals
     {
         public const string k_Title = "Heap Explorer";
-        public const string k_Version = "alpha 2.2";
+        public const string k_Version = "alpha 2.3";
         public const string k_DocuUrl = "http://www.console-dev.de/bin/HeapExplorer.pdf";
         public const string k_ForumUrl = "https://forum.unity.com/threads/wip-heap-explorer-memory-profiler-debugger-and-analyzer-for-unity.527949/";
         public const string k_PublisherUrl = "https://www.assetstore.unity3d.com/en/#!/search/page=1/sortby=popularity/query=publisher:3683";
