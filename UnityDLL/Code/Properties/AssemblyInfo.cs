75f30a41a3e0e672ae7aef8665d4fe6887802865
Peter Schraut <p-bitbucket@console-dev.de>
2019-02-24 16:35:38 +0100

    Updated UnityDLL project

--- UnityDLL/Code/Properties/AssemblyInfo.cs
+++ UnityDLL/Code/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿//
+// Heap Explorer for Unity. Copyright (c) 2019 Peter Schraut (www.console-dev.de). See LICENSE.md
+// https://bitbucket.org/pschraut/unityheapexplorer/
+//
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using UnityEngine;
@@ -8,7 +12,7 @@ using UnityEngine;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Peter Schraut")]
 [assembly: AssemblyProduct("HeapExplorer")]
-[assembly: AssemblyCopyright("Copyright 2017-2018 Peter Schraut")]
+[assembly: AssemblyCopyright("Copyright 2017-2019 Peter Schraut")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
