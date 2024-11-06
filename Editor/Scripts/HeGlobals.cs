369b3b492c011194c3504551f0e9a47b84e3100d
pschraut <p-github@console-dev.de>
2024-11-06 20:00:07 +0100

    Fixed version number

--- Editor/Scripts/HeGlobals.cs
+++ Editor/Scripts/HeGlobals.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Heap Explorer for Unity. Copyright (c) 2019-2021 Peter Schraut (www.console-dev.de). See LICENSE.md
+// Heap Explorer for Unity. Copyright (c) 2019-2024 Peter Schraut (www.console-dev.de). See LICENSE.md
 // https://github.com/pschraut/UnityHeapExplorer/
 //
 using System.Collections;
@@ -11,7 +11,7 @@ namespace HeapExplorer
     public static class HeGlobals
     {
         public const string k_Title = "Heap Explorer";
-        public const string k_Version = "4.1";
+        public const string k_Version = "4.2";
         public const string k_DocuUrl = "https://github.com/pschraut/UnityHeapExplorer";
         public const string k_ChangelogUrl = "https://github.com/pschraut/UnityHeapExplorer/blob/master/CHANGELOG.md";
         public const string k_ForumUrl = "https://forum.unity.com/threads/wip-heap-explorer-memory-profiler-debugger-and-analyzer-for-unity.527949/";
